324273848917dc5153adff57ed0017d2366e09ed
root <root@damoncheng.www.damoncheng.com>
2018-03-06 03:09:33 +0800

    complete docker cluster version 1

--- Docker/Docker_Instance/Jenkins/jenkins/jenkins-service.sh
+++ Docker/Docker_Instance/Jenkins/jenkins/jenkins-service.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+#-----------Normal----------#
+
+
 #create swarm network
 docker network create -d overlay jenkins_master_net
 
+#--virsualizer--#
 #create visualizer docker
 docker service create --name visualizer --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock --constraint 'node.role==manager' --network jenkins_master_net --publish 8000:8080 dockersamples/visualizer:stable
 
+#--jenkins_master--#
 #create jenkins_master docker
 docker service create --user root --name jenkins_master --mount type=bind,source=/var/jenkins_home,destination=/var/jenkins_home --constraint 'node.role==worker' --restart-condition='on-failure'  --network jenkins_master_net --publish 36093:36093 --publish 8080:8080 damoncheng/public:jenkins
 
@@ -20,4 +25,25 @@ docker service create --user root --name jenkins_master --mount type=volume,sour
 
 #create jenkins blueocean_master docker by bind and volumn
 docker service create --user root --name jenkins_master --mount type=bind,source=/home/damoncheng,destination=/home/damoncheng --mount type=volume,source=jenkins_home,destination=/var/jenkins_home --constraint 'node.role==manager' --restart-condition='on-failure' -e JAVA_OPTS=-Duser.timezone=Hongkong --network jenkins_master_net --publish 36093:50000 --publish 8080:8080 damoncheng/public:jenkinsci-blueocean
+
+#create jenkins blueocean_master docker by bind and volumn and timer  我的工作环境
+docker service create --user root --name jenkins_master --mount type=bind,source=/tmp,destination=/tmp --mount type=volume,source=jenkins_home,destination=/var/jenkins_home --constraint 'node.role==manager' --restart-condition='on-failure' -e JAVA_OPTS=-Duser.timezone=Hongkong --network jenkins_master_net --publish 36093:50000 --publish 8080:8080 damoncheng/jenkins:blueocean-timer
+
+#create jenkins blueocean_master docker by bind and bind and timer
+docker service create --user root --name jenkins_master --mount type=bind,source=/tmp,destination=/tmp --mount type=bind,source=/var/jenkins_home,destination=/var/jenkins_home --constraint 'node.role==manager' --restart-condition='on-failure' -e JAVA_OPTS=-Duser.timezone=Hongkong --network jenkins_master_net --publish 36093:50000 --publish 8080:8080 damoncheng/jenkins:blueocean-timer
+
+#--rsync and inotify--#
+docker service create --user root -e "IP_LIST=`cat ip.list`" --name jenkins_master_storage --mount type=bind,source=/tmp,destination=/tmp --mount type=volume,source=jenkins_home_timer,destination=/var/jenkins_home --constraint 'node.role==worker' --mode global --restart-condition='on-failure' --publish 873:873  damoncheng/jenkins:sync-2
+
+
+#-------------Test-----------#
+#create jenkins blueocean_master docker by bind and add a timer
+docker service create --user root --name jenkins_master --mount type=bind,source=/tmp,destination=/tmp --mount type=volume,source=jenkins_home_timer,destination=/var/jenkins_home --constraint 'node.role==worker' --restart-condition='on-failure' -e JAVA_OPTS=-Duser.timezone=Hongkong --network jenkins_master_net --publish 36093:50000 --publish 8080:8080 damoncheng/jenkins:blueocean-timer
+
+
+docker service create --user root -e "IP_LIST=`cat ip.list`" --name jenkins_master_storage --mount type=bind,source=/tmp,destination=/tmp --mount type=volume,source=jenkins_home_timer,destination=/var/jenkins_home --constraint 'node.role==worker' --mode global --restart-condition='on-failure' --publish 873:873  damoncheng/test:rsyncd
+
+
+docker --user root -e "IP_LIST=`cat ip.list`"  run damoncheng/test:rsyncd
+
 
