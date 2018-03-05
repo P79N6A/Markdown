b617e7ac3f5c2e391cbe45ca0e53ab9772329a7f
root <root@damoncheng.www.damoncheng.com>
2018-03-06 03:09:33 +0800

    complete docker cluster version 1

--- Docker/Docker_Instance/Cluster_Master/cluster/core/sync.sh
+++ Docker/Docker_Instance/Cluster_Master/cluster/core/sync.sh
@@ -12,8 +12,10 @@ exclude=$8
 #echo $exclude
 #exit 0
 
-log_data=`rsync -r -v -l -H -p -o -g -D -t -S -u --delete --password-file=/etc/rsyncd.secrets  $exclude  \
+log_data=`rsync -r -v -l -H -p -o -g -D -t -S -u --delete --contimeout=1 --password-file=/etc/rsyncd.secrets  $exclude  \
       $src $user@$ip::$dest` 
 
-$logfile $logdir/${logprefix}_rsync_log_${ip}_${dest} "$log_data"
+echo "`date`=>$ip : $log_data"
+
+#$logfile $logdir/${logprefix}_rsync_log_${ip}_${dest} "$log_data"
 
